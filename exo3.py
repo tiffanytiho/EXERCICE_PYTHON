34f7c44271dbb62868ffe80acbc99b8d3fe54ada
tiffanytiho <135434318+tiffanytiho@users.noreply.github.com>
2023-08-14 02:26:37 +0000

    Exo3

---
+++ exo3.py
@@ -0,0 +1,9 @@
+# Exo 3 : Lire un fichier texte dans une variable et remplacer toutes les nouvelles lignes par des espaces
+chaine1 = input (" Saisissez une première chaine de caratère")
+chaine2 = input (" Saisissez une deuxième chaine de caratère")
+chaine3 = input (" Saisissez une troisième chaine de caratère")
+
+result = chaine1 + " " + chaine2 + " " + chaine3
+print('La concatenation de vos phrases donne :', result) 
+
+
