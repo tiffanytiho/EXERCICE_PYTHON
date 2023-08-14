649d81dd473c24ddf4d0e5a84b39b9a43e3794c1
tiffanytiho <135434318+tiffanytiho@users.noreply.github.com>
2023-08-14 02:00:57 +0000

    Exercice 1

---
+++ exo1.py
@@ -0,0 +1,7 @@
+print ("Hello , world")
+
+# Exo 1 : Création d'un programme qui change l’ordre des éléments d’une liste en les renversant.
+Liste_nombre = [1, 2, 3, 4]
+Liste_nombre.reverse()
+
+print(Liste_nombre)  
